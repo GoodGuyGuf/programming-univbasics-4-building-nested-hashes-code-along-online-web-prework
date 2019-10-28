5258b888c925ebdd86a2b2a36da51ac80c46fd15
Ethan <ethan_gustafson@outlook.com>
2019-10-28 05:27:20 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,11 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
       }, 
+      :matriarch => {
     :capulet => {
       #your key/value pairs here!
       }}

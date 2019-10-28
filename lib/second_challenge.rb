0e7dbd67d0f7e2e88e29db9643eb02cf0401e413
Ethan <ethan_gustafson@outlook.com>
2019-10-28 05:33:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,6 @@ def second_challenge
     :capulet => {
       #your key/value pairs here!
       :patriarch => {}
-      :patriarch => {}, 
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
